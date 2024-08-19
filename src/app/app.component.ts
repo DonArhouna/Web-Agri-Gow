6829218e9c638a9d5559b1c82d2b3205cb2ceb92
K-Arouna <ronekane94@gmail.com>
2024-08-19 13:34:49 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Web-Agri-Gov';
+
+export class AppComponent implements OnInit {
+  title = 'Welcome';
+  localites: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getLocalites().subscribe(data => {
+      this.localites = data;
+    });
+  }
 }
+
